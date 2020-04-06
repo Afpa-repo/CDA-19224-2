0f6c836d360bf644a691457c81a2ba625ccea29b
guillaume <guillaume.kieffer.pro@gmail.com>
2020-04-06 14:26:32 +0200

    Update Front

---
+++ GreenAndCare/src/Controller/Admin/AdminProductController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminProductController extends AbstractController
+{
+    /**
+     * @var ProductRepository
+     */
+    private $repository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(ProductRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/admin", name="admin.product.index")
+     * @return Response
+     */
+    public function index()
+    {
+        $products = $this->repository->findAll();
+        return $this->render('admin/product/index.html.twig', compact('products'));
+    }
+
+    /**
+     * @Route("/admin/product/create", name="admin.product.new")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request)
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($product);
+            $this->em->flush();
+            $this->addFlash('success', 'Le produit a bien été ajouté');
+            return $this->redirectToRoute('admin.product.index');
+        }
+
+        return $this->render('admin/product/new.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/{id}", name="admin.product.edit", methods="GET|POST")
+     * @param Product $product
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Product $product, Request $request)
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            $this->addFlash('success', 'Le produit a bien été modifié');
+            return $this->redirectToRoute('admin.product.index');
+        }
+
+        return $this->render('admin/product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/{id}", name="admin.product.delete", methods="DELETE")
+     * @param Product $product
+     * @param Request $request
+     * @return Response
+     */
+    public function delete(Product $product, Request $request)
+    {
+        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->get('_token'))) {
+            $this->em->remove($product);
+            $this->em->flush();
+            $this->addFlash('success', 'Le produit a bien été supprimé');
+
+        }
+
+        return $this->redirectToRoute('admin.product.index');
+    }
+}
---
+++ GreenAndCare/src/Form/ProductType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('short_description')
+            ->add('long_description')
+            ->add('price', MoneyType::class, [
+                'currency' => 'EUR'
+            ])
+            ->add('soldout')
+            ->add('promo')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+            'translation_domain' => 'forms'
+        ]);
+    }
+}
